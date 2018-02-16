ad25fd04e4639cc8fd6acf1a0df69720499374eb
peterbell10 <peterbell10@live.co.uk>
2018-02-16 12:33:28 +0000

    Fix minor code errors

--- simulator.py
+++ simulator.py
@@ -3,9 +3,9 @@ import scipy.sparse as sp
 import numpy as np
 
 def apply_gate(gate, register):
-    if gate.gate_type = basis_gates.hadamard:
+    if gate.gate_type == basis_gates.hadamard:
         return apply_hadamard(gate, quantum_register)
-    elif gate.gate_type = basis_gates.contr_phase:
+    elif gate.gate_type == basis_gates.contr_phase:
         return apply_c_phase(gate, quantum_register)
 
 
@@ -14,15 +14,15 @@ def apply_hadamard(gate, register):
 
 
 def apply_square_matrix(mat, register, qbits):
-    assert len(mat.shape) = 2          # Matrix
-    assert mat.shape[0] = mat.shape[1] # Square
+    assert len(mat.shape) == 2          # Matrix
+    assert mat.shape[0] == mat.shape[1] # Square
     size = len(register)
     w = np.zeros(size)
     for i in range(0, size):
-        r = gather(i)
-        i0 = i & ~scatter(r)
+        r = gather(i, qbits)
+        i0 = i & ~scatter(r, qbits)
         for c in range(0, mat.shape[0]):
-            j = i0 | scatter(c)
+            j = i0 | scatter(c, qbits)
             w[i] = w[i] + mat[r, c] * register[j]
     return w
 
@@ -32,7 +32,7 @@ def gather(i, qbits):
         j = j | (((i >> qb_pos) & 1) << k)
     return j
 
-def scatter(j):
+def scatter(j, qbits):
     i = 0
     for k, qb_pos in enumerate(qbits):
         i = i | (((j << k) & 1) << qb_pos)
