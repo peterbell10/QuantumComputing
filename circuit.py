6255e966932d03d5889dd1de180746b557d18c3e
peterbell10 <peterbell10@live.co.uk>
2018-02-24 19:08:53 +0000

    Remove composite gates from circuit.py

--- circuit.py
+++ circuit.py
@@ -76,26 +76,5 @@ def hadamard(qbit):
 def c_phase(control_qbit, phase_qbit, phase):
     return circuit()._add_gate(controlled_phase_gate(control_qbit, phase_qbit, phase))
 
-def c_not(control_qbit, not_qbit):
-    return (
-        hadamard(not_qbit) |
-        c_phase(control_qbit, not_qbit, 0.5) |
-        hadamard(not_qbit))
-
-def toffoli(control_1_qbit, control_2_qbit, not_qbit):
-    return (
-        hadamard(not_qbit) |
-        c_phase(control_2_qbit, not_qbit, 0.25) |
-        c_not(control_1_qbit, control_2_qbit) |
-        c_phase(control_2_qbit, not_qbit, -0.25) |
-        c_not(control_1_qbit, control_2_qbit) |
-        c_phase(control_1_qbit, not_qbit, 0.25) |
-        hadamard(not_qbit))
-
-def adder(x_qbit, y_qbit, carry_qbit):
-    return (
-        toffoli(x_qbit, y_qbit, carry_qbit) |
-        c_not(x_qbit, y_qbit))
-
 if __name__ == '__main__':
     print(str(adder(1, 2, 3)))
